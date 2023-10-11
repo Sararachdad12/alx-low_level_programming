d910025e170b39c7fd606048dd88f9a6fa0d4a5c
Sararachdad12 <sarashanaz36@gmail.com>
2023-10-10 17:14:32 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- */
+ * jack_bauer - prints every minute of the day
+*/
+
 void jack_bauer(void)
 {
 	int hours, minutes;
@@ -20,4 +20,3 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-
